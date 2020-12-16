fdc33a9f94077ecf00c0d119b39038c40e1116a9
rchoe1229 <ryan.choe@outlook.com>
2020-12-15 17:00:19 -0700

    commit tree 268615d76e86f411db8f6249d3fc067d3f5cecfb

--- app.rb
+++ app.rb
@@ -1,16 +1,62 @@
+require 'pry'
+
 class Company
     attr_accessor :name, :size
+
+    @@all = []
 
     def initialize(name, size)
         @name = name
         @size = size
+        @@all << self
     end
-end
 
 companies = [
     Company.new('Alpha', 30),
     Company.new('Beta', 300),
-    Company.new('Delta', 3000)
+    Company.new('Delta', 3000),
+    Company.new('Charlie', 4000)
 ]
 
 ### Your code below this
+
+    def self.all
+        @@all
+    end
+    
+    def self.companies_info
+        all.map do |company|
+            "#{company.name} - #{company.size}"
+            #String Interpolation ^^
+        end
+    end
+
+    def self.size_sum
+        all.reduce(0) do |total, company|
+            total + company.size
+        end
+    end
+
+    def self.size_greater_than_100
+        all.select do |company|
+            company.size > 100
+        end
+    end
+
+    def self.beta 
+        all.find do |companies|
+            companies.name == 'Beta'
+        end
+    end
+
+    def self.biggest
+        all.max_by {|company| company.size}
+    end
+
+    def self.size_sort
+        sorted = all.sort_by {|company| company.size}
+    sorted.reverse
+    end
+end
+
+binding.pry
